4bc6472c0873be89a6fad0708cf04e79f6fea5b6
KamB-t <kbende2@lsu.edu>
2026-09-15 16:00:21 -0500

    Added new test*

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -69,4 +69,22 @@ void main() {
     // Verify that our counter has reset to zero.
     expect(tester.widget<Text>(counter).data, '0');
   });
+
+  testWidgets('Counter incremental (three)', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    final counter = find.byKey(const Key('counter-value'));
+    expect(tester.widget<Text>(counter).data, '0');
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(tester.widget<Text>(counter).data, '3');
+  });
 }
